a04c1c727babad2bbb502ad20bfc56d1bdb0de44
Mamoru OKAMOTO <okamoto@portable-debian>
2013-09-27 13:27:11 +0900

    2013/9/27

--- Graphics/GPDriver/test.c
+++ Graphics/GPDriver/test.c
@@ -1,18 +1,28 @@
-#include "GPDriver.h"
+#include <time.h>
 #include <unistd.h>
+#include <math.h>
+#include "GPDriver.h"
 
 int main()
 {
 	double min = -1, max = 1;
-	int dim = 400;
-	double xy[2*dim];
+	int dim = 10;
+	double xy[3*dim*dim], x, y;
 	for(int i = 0; i < dim; ++i)
-		xy[2*i] = i*(max - min)/(dim - 1) + min, xy[2*i + 1] = xy[2*i]*xy[2*i];
+	for(int j = 0; j < dim; ++j)
+		xy[3*(j*dim + i)] = x = i*(max - min)/(dim - 1) + min, xy[3*(j*dim + i) + 1] = y = j*(max - min)/(dim - 1) + min, xy[3*(j*dim + i) + 2] = 4*sin(1/(1 + sqrt(x * x + y * y))) - 2.5 ;
 	GPData gpd = CreateGPData();
-	SetRange(&gpd, min, max, -0.2, 3);
+	for(int i = 0; i < 3; ++i)
+		SetRange(&gpd, min, max, i);
+	SetWindow(&gpd, 0, 0, 800, 600);
+	ClearFlags(&gpd, withPoint | withLine);
+	SetFlags(&gpd, withLine);
 	InitializeGP(&gpd);
-	Plot(&gpd, xy, dim);
-	sleep(10);
+	while(1)
+	{
+		SPlot(&gpd, xy, dim, dim);
+		sleep(1);
+	}
 	DeleteGPData(&gpd);
 	return 0;
 }
