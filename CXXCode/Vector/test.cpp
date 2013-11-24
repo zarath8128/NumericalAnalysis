0609fe223136862020f5e0d89df5472ccdce22f1
zarath8128 <zarath8128@gmail.com>
2013-11-24 19:27:48 +0900

    2013/11/124

--- CXXCode/Vector/test.cpp
+++ CXXCode/Vector/test.cpp
@@ -7,6 +7,7 @@ using namespace zarath::Vector;
 int main()
 {
 	constexpr uint64_t dim = 40000000;
+	constexpr uint64_t times = 40;
 	double *s = new double[dim], *t = new double[dim];
 	Vector<dim> v, w;
 	for(uint64_t i = 0; i < dim; ++i)
@@ -16,9 +17,11 @@ int main()
 	}
 
 	clock_t t0 = clock();
+	for(uint64_t time = 0; time< times; ++time)
 	for(uint64_t i = 0; i < dim; ++i)
 		v[i] = v[i] + w[i];
 	clock_t t1 = clock();
+	for(uint64_t time = 0; time < times; ++time)
 	for(uint64_t i = 0; i < dim; ++i)
 		s[i] = s[i] + t[i];
 	clock_t t2 = clock();
