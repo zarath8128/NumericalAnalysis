9f3e423afacb27b88e558bdae893b2b7ff3c18a1
zarath <okamoto6496@gmail.com>
2013-11-24 17:57:09 +0900

    2013/11/24 home

--- CXXCode/Vector/Vector.hpp
+++ CXXCode/Vector/Vector.hpp
@@ -23,42 +23,56 @@ namespace zarath
 
 		template<class F, class N, class V, uint64_t dim>
 		class NumericVector
-			:AbstractVector<F, V>
+			:public AbstractVector<F, V>
 		{
 		public:
 			virtual N &operator[](uint64_t index) = 0;
-			constexpr virtual operator uint64_t() final {return dim;}
+			constexpr virtual explicit operator uint64_t() final {return dim;}
 		};
 
 		template<uint64_t dim>
 		class Vector
-			:NumericVector<double, double, Vector<dim>, dim>
+			:public NumericVector<double, double, Vector<dim>, dim>
 		{
 		public:
-			Vector():val(new double[dim]){std::cout << "construct\n";}
-			Vector(Vector &a, Vector &b, void (*ope)(double *, double *, double *)):Vector(){ope(a.val, b.val, val);}
-			Vector(const Vector &v):Vector(){for(uint64_t i = 0; i < dim; ++i)val[i] = v.val[i];std::cout << "copy cons\n";}
-			Vector(Vector &&v):val(v.val){v.val = nullptr;std::cout << "move cons\n";}
-			~Vector(){delete [] val; std::cout << "destruct\n";}
+			Vector():val(new double[dim]){}
+			Vector(double *a, double *b, void (*ope)(double *, double *, double *)):Vector(){ope(a, b, val);}
+			Vector(const Vector &v):Vector(){for(uint64_t i = 0; i < dim; ++i)val[i] = v.val[i];}
+			Vector(Vector &&v):val(v.val){v.val = nullptr;}
+			~Vector(){delete [] val;}
 
 			double &operator[](uint64_t index){return val[index];} 
-			const Vector &operator=(Vector &v){for(uint64_t i = 0; i < dim; ++i)val[i] = v[i];std::cout << "copy" << std::endl;return *this;}
-			const Vector &operator=(Vector &&v){val = v.val; v.val = nullptr; std::cout << "move" << std::endl;return *this;}
+			const Vector &operator=(Vector &v){for(uint64_t i = 0; i < dim; ++i)val[i] = v[i];return *this;}
+			const Vector &operator=(Vector &&v){val = v.val; v.val = nullptr;return *this;}
 			bool operator==(Vector &v){for(uint64_t i = 0; i < dim; ++i)if(v[i] != val[i])return false;return true;}
 			bool operator!=(Vector &v){return !(*this == v);}
-			Vector operator+(Vector &v){std::cout << "add" << std::endl;return Vector(*this, v, Add);}
+			Vector operator+(Vector &v){return Vector(val, v.val, Add);}
 			const Vector &operator+=(Vector &v){for(uint64_t i = 0; i < dim; ++i)val[i] += v[i]; return *this;}
-			Vector operator*(double &v){Vector<dim> ret;for(uint64_t i = 0; i < dim; ++i)ret[i] = val[i]*v; return ret;}
+			Vector operator-(Vector &v){return Vector(val, v.val, Sub);}
+			const Vector &operator-=(Vector &v){for(uint64_t i = 0; i < dim; ++i)val[i] -= v[i]; return *this;}
+			Vector operator*(double &v){return Vector(val, &v, Mul);}
+			Vector operator*(double v){return Vector(val, &v, Mul);}
 			const Vector &operator*=(double &v){for(uint64_t i = 0; i < dim; ++i)val[i] *= v; return *this;}
 		private:
 			static void Add(double *a, double *b, double *ans){for(uint64_t i = 0; i < dim; ++i)ans[i] = a[i] + b[i];}
+			static void Sub(double *a, double *b, double *ans){for(uint64_t i = 0; i < dim; ++i)ans[i] = a[i] - b[i];}
+			static void Mul(double *a, double *b, double *ans){for(uint64_t i = 0; i < dim; ++i)ans[i] = a[i] * *b;}
 			double *val;
 		};
 
 		template<uint64_t dim>
 		std::ostream &operator<<(std::ostream &dest, Vector<dim> &src)
 		{
-			for(int i = 0; i < dim - 1; ++i)
+			for(uint64_t i = 0; i < dim - 1; ++i)
+				dest << src[i] << "\n";
+			dest << src[dim - 1];
+			return dest;
+		}
+
+		template<uint64_t dim>
+		std::ostream &operator<<(std::ostream &dest, Vector<dim> &&src)
+		{
+			for(uint64_t i = 0; i < dim - 1; ++i)
 				dest << src[i] << "\n";
 			dest << src[dim - 1];
 			return dest;
--- CXXCode/Vector/test.cpp
+++ CXXCode/Vector/test.cpp
@@ -1,12 +1,33 @@
 #include "Vector.hpp"
+#include <iostream>
+#include <ctime>
 
 using namespace zarath::Vector;
 
 int main()
 {
-	Vector<40> v, w;
-	for(int i = 0; i < 4; ++i)
-		v[i] = i*i*0.1, w[i] = i + 0.2;
-	Vector<40> u = v + w;
+	constexpr uint64_t dim = 40000000;
+	double *s = new double[dim], *t = new double[dim];
+	Vector<dim> v, w;
+	for(uint64_t i = 0; i < dim; ++i)
+	{
+		v[i] = s[i] = 0;
+		w[i] = t[i] = 1./i;
+	}
+
+	clock_t t0 = clock();
+	for(uint64_t i = 0; i < dim; ++i)
+		v[i] = v[i] + w[i];
+	clock_t t1 = clock();
+	for(uint64_t i = 0; i < dim; ++i)
+		s[i] = s[i] + t[i];
+	clock_t t2 = clock();
+
+	std::cout << "Vector   time = " << (t1 - t0)/(double)CLOCKS_PER_SEC << std::endl;
+	std::cout << "double[] time = " << (t2 - t1)/(double)CLOCKS_PER_SEC << std::endl;
+	
+	delete [] s;
+	delete [] t;
+
 	return 0;
 }
